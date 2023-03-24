6e1aa4c335bfdf3c005ed40e3d38d2c824c0c2fb
egiho <dorkassinambela85@gmail.com>
2023-03-24 19:51:03 +0700

    first commit

---
+++ animal.js
@@ -0,0 +1,8 @@
+// Import class Animal dari file Animal.ts
+import { Animal } from "./Animal";
+
+// Membuat instance baru dari class Animal
+const cat = new Animal("cat");
+
+// Memanggil method makeSound dari instance cat
+cat.makeSound(); // Output: cat makes a sound
---
+++ animal.ts
@@ -0,0 +1,11 @@
+class Animal {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  makeSound(): void {
+    console.log(`${this.name} makes a sound`);
+  }
+}
